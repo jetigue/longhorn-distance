d36c1ee2bf159ecfc717df91ec1a3039d43ed838
James <jetigue@comcast.net>
2017-07-27 17:29:35 -0400

    Fixed admin menu

--- resources/views/layouts/menus/admin_menu.blade.php
+++ resources/views/layouts/menus/admin_menu.blade.php
@@ -17,26 +17,26 @@
   </p>
   <ul class="menu-list">
     <li>
-      <div v-on:click="show = !show" class="dropdown-label">
+      <div class="dropdown-label">
         <a>Users</a> 
-          <i class="fa" :class="{ fa-plus: isPlus, fa-minus: isMinus}" style="font-size:12px;" aria-hidden="true" @click="toggleClass"></i>
-          <!-- <i class="fa fa-minus" style="font-size:12px;" aria-hidden="true"></i> -->
+          <i class="fa fa-plus" style="font-size:12px;" aria-hidden="true"></i>
+          <i class="fa fa-minus" style="font-size:12px;" aria-hidden="true"></i>
       </div>
    
-      <ul v-if="show">
+      <ul>
         <li><a href="{{ url('/admin/users/user_roles') }}">User Roles</a></li>
         <li><a href="{{ url('/admin/users/users') }}">Users</a></li>
       </ul>
     </li>
 
     <li>
-      <div v-on:click="show = !show" class="dropdown-label">
+      <div class="dropdown-label">
         <a>Running Log</a> 
-          <i v-if="!show" class="fa fa-plus" style="font-size:12px;" aria-hidden="true"></i>
-          <i v-else="show" class="fa fa-minus" style="font-size:12px;" aria-hidden="true"></i>     
+          <i class="fa fa-plus" style="font-size:12px;" aria-hidden="true"></i>
+          <i class="fa fa-minus" style="font-size:12px;" aria-hidden="true"></i>     
       </div>
 
-      <ul v-if="show">
+      <ul>
         <li><a href="{{ url('/admin/running_log/day_times') }}">Day Times</a></li>
         <li><a href="{{ url('/admin/running_log/run_efforts') }}">Run Efforts</a></li>
         <li><a href="{{ url('/admin/running_log/run_feelings') }}">Run Feeling</a></li>
