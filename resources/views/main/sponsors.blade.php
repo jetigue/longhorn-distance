7de8c1c7f23847e61e2e8dd8ebdbbc5684ac216a
James <jetigue@comcast.net>
2017-08-01 22:06:27 -0400

    Added Sponsors and Logos

--- resources/views/main/sponsors.blade.php
+++ resources/views/main/sponsors.blade.php
@@ -2,19 +2,134 @@
 
 @section('content')
 
-<h1 class="title">
-  Please support our sponsors that support our team!
-</h1>
+<div class="content main-content-header">
+Please Support our Sponsors!
+</div>
+
+<div class="columns">
+	<div class="column is-4 is-offset-4">
+		<div class="card">
+		  <div class="card-image">
+		    <figure class="image is-4by3">
+		      <a href="http://www.neesejones.com/"><img src="{{url('images/sponsors/2017/NeeseJones.png')}}" alt="Neese Jones"></a>
+		    </figure>
+		  </div>
+		  <div class="card-content ">
+		    <div class="content has-text-centered">
+			<strong>Platinum Level</strong>
+		    </div>
+		  </div>
+		</div>
+	</div>
+</div>
+
+<div class="columns">
+	<div class="column is-3">
+		<div class="card">
+		  <div class="card-image">
+		    <figure class="image is-square">
+		      <a href="http://sfanimalhospital.com/"><img src="{{url('images/sponsors/2017/SFAH.png')}}" alt="South Forsyth Animal hopsital"></a>
+		    </figure>
+		  </div>
+		  <div class="card-content ">
+		    <div class="content has-text-centered">
+			<strong>Gold Level</strong>
+		    </div>
+		  </div>
+		</div>
+	</div>
+
+	<div class="column is-3">
+		<div class="card">
+		  <div class="card-image">
+		    <figure class="image is-square">
+		      <a href="http://www.northatlantaprimarycare.com/"><img src="{{url('images/sponsors/2017/NAPC.png')}}" alt="North Atlanta Primary Care"></a>
+		    </figure>
+		  </div>
+		  <div class="card-content ">
+		    <div class="content has-text-centered">
+			<strong>Gold Level</strong>
+		    </div>
+		  </div>
+		</div>
+	</div>
+
+	<div class="column is-3">
+		<div class="card">
+		  <div class="card-image">
+		    <figure class="image is-square">
+		      <a href="https://sportscarephysicaltherapy.com/"><img src="{{url('images/sponsors/2017/SCPT.png')}}" alt="Sports care Physical Therapy"></a>
+		    </figure>
+		  </div>
+		  <div class="card-content ">
+		    <div class="content has-text-centered">
+			<strong>Gold Level</strong>
+		    </div>
+		  </div>
+		</div>
+	</div>
+
+	<div class="column is-3">
+		<div class="card">
+		  <div class="card-image">
+		    <figure class="image is-square">
+		      <a href="http://crystalcc.com/"><img src="{{url('images/sponsors/2017/Crystal.png')}}" alt="Franklin Technologies"></a>
+		    </figure>
+		  </div>
+		  <div class="card-content ">
+		    <div class="content has-text-centered">
+			<strong>Gold Level</strong>
+		    </div>
+		  </div>
+		</div>
+	</div>
+</div>
 
 <div class="columns">
 	<div class="column is-4">
-	Platinum
+		<div class="card">
+		  <div class="card-image">
+		    <figure class="image is-16by9">
+		      <a href="https://www.resurgens.com/johns-creek"><img src="{{url('images/sponsors/2017/Resurgens.png')}}" alt="Resurgens Othopaedics"></a>
+		    </figure>
+		  </div>
+		  <div class="card-content ">
+		    <div class="content has-text-centered">
+			<strong>Gold Level</strong>
+		    </div>
+		  </div>
+		</div>
 	</div>
 
-	<div class="column" id="neese-jones-container">
+	<div class="column is-4">
+		<div class="card">
+		  <div class="card-image">
+		    <figure class="image is-16by9">
+		      <a href="https://www.cmitsolutions.com/"><img src="{{url('images/sponsors/2017/CMIT.png')}}" alt="CMIT Solutions"></a>
+		    </figure>
+		  </div>
+		  <div class="card-content ">
+		    <div class="content has-text-centered">
+			<strong>Gold Level</strong>
+		    </div>
+		  </div>
+		</div>
 	</div>
 
-	
+	<div class="column is-4">
+		<div class="card">
+		  <div class="card-image">
+		    <figure class="image is-16by9">
+		      <a href="http://cumminggahomes.yourkwagent.com/"><img src="{{url('images/sponsors/2017/Mahota.png')}}" alt="Mahota Real Estate Partners"></a>
+		    </figure>
+		  </div>
+		  <div class="card-content ">
+		    <div class="content has-text-centered">
+			<strong>Gold Level</strong>
+		    </div>
+		  </div>
+		</div>
+	</div>
 </div>
 
 @endsection
