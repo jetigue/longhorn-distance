faf83109601e1396b23fed807744f610980fc5e4
James <jetigue@comcast.net>
2017-09-04 21:14:27 -0400

    updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,17 +10,12 @@
       <div class="tile is-parent">
         <article class="tile is-child box">
           <div class="content announcements">
-            <h4>Lambert River Run</h4>
-            <p><strong>THANK YOU PARENT VOLUNTEERS</strong> And to all of the runners and everyone involved in making the meet run smoothly!</p>
-            <p>Congratulations to the varsity boys for their first place finish! Also, special recognition goes out to <strong>Ty Fox</strong> for setting a new school record in the 5k with a winning time of <strong>15:35.80</strong>!</p>
-            <p><a href="http://ga.milesplit.com/meets/279676/results#.WaL0eyh96Uk">Complete Lambert River Run Results</a></p>
-
-
-            <h4>Berry Invitational</h4>
-            <p>The Berry Invitational will be this Saturday, September 2nd. Please email Coach Tigue ASAP if you are unable to attend.</p>
-            <p><a href="http://ga.milesplit.com/meets/283254/info#.WaL62Sh96Uk">Berry Invitational Information Page</a></p>
-            <p><strong>Busses will depart from the stadium parking lot at 5:45 am. Please be there at 5:30 am. </strong>We should return to the school stadium lot around 1:30 pm.</p>
-
+            <h4>Furman Invitational</h4>
+            <p>The Furman Invitational will be this Saturday, September 9th. Summer running program athletes will be participating in the meet. <a href="https://drive.google.com/open?id=1dwQr4vpPQmENShAjkx3uD9VMXId2_ew8d_RRVDqSUIc">Summer Running Qualifiers</a></p>
+            <p>Students will be dimissed from class at 2:00 pm on Friday. The bus will depart at 2:20 pm from the stadium parking lot.</p>
+            <p><a href="http://sc.milesplit.com/meets/265145/info#.Wa3w8siGOUk">Furman Meet Information</a></p>
+            <h4>Afternoon Practices Begin!</h4>
+            <p>We will now meet in the afternoons at 3:55 pm in the stadium for all cross country practices, unless otherwise noted. Practice will last until 5:30 pm. Please arrange to be picked up from practice at that time. Be sure to stay hydrated throughout the day. Make sure you have a watch and a water bottle for practice each day.</p>
           </div>
         </article>
       </div>
@@ -31,8 +26,13 @@
           <div class="tile is-parent">
             <article class="tile is-child box">
               <p class="title">Latest Results</p>
-              <p class="subtitle">Lambert River Run</p>
-              <div class="content">
+              <div class="content events">
+                <h4>Berry Invitational</h4>
+                <h6><a href="http://ga.milesplit.com/meets/283254/results/535785/raw#.Wa31hch96Uk">Varsity Boys: 4th</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/283254/results/535773/raw#.Wa31nch96Uk">Varsity Girls: 9th</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/283254/results/535788/formatted#.Wa31vch96Uk">JV Boys: 5th</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/283254/results/535788/formatted#.Wa31vch96Uk">JV Girls: 3rd</a></h6>
+                <h4>Lambert River Run</h4>
                 <h6><a href="http://ga.milesplit.com/meets/279676/results/534569/raw#.WaMAmCh96Uk">Varsity Boys: 1st!</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/279676/results/534563/raw#.WaMBfih96Uk">Varsity Girls: 4th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/279676/results/534565/raw#.WaMBnyh96Uk">JV Boys: 5th</a></h6>
@@ -50,13 +50,15 @@
         <article class="tile is-child box">
           <p class="title">Girls Varsity ROW</p>
           <div class="content">
-            <p><strong>Caroline Gaharan</strong>, top Longhorn finisher (6th overall) and fastest freshman in the field! (19:33.89)</p>
+          <p><strong>Emily Chapman</strong>, freshman, finished third for the Longhorns, running just as fast on a slower course than last week (21:15).</p>
+<!--             <p><strong>Caroline Gaharan</strong>, top Longhorn finisher (6th overall) and fastest freshman in the field! (19:33.89)</p> -->
           </div>
         </article>
         <article class="tile is-child box">
           <p class="title">Girls JV ROW</p>
           <div class="content">
-            <p><strong>Molly Garrett</strong>, 3rd place individually with a time of 22:47 leading the Longhorns to a 3rd place team finish!</p>
+            <p><strong>Kaylee Stillwell</strong>, 5th place individually with a time of 22:59 leading the Longhorns to a 3rd place team finish!</p>
+<!--             <p><strong>Molly Garrett</strong>, 3rd place individually with a time of 22:47 leading the Longhorns to a 3rd place team finish!</p> -->
           </div>
         </article>
       </div>
@@ -64,13 +66,15 @@
       <article class="tile is-child box">
           <p class="title">Boys Varsity ROW</p>
           <div class="content">
-            <p><strong>Ty Fox</strong>, individual River Run champion and school record setter in the 5k with a time of 15:35.80!</p>
+           <p><strong>Adam Nicholason</strong>, finished 4th for the Longhorns and 3rd freshman in the field with a time of 17:41.</p>
+<!--             <p><strong>Ty Fox</strong>, individual River Run champion and school record setter in the 5k with a time of 15:35.80!</p> -->
           </div>
         </article>
         <article class="tile is-child box">
           <p class="title">Boys JV ROW</p>
           <div class="content">
-            <p><strong>Evan Shadoff</strong>, 15th place in the JV race with a new PR of 19:12!</p>
+          <p><strong>Jack Frager</strong>, 18th place in the JV race with a time of 18:51.</p>
+           <!--  <p><strong>Evan Shadoff</strong>, 15th place in the JV race with a new PR of 19:12!</p> -->
           </div>
         </article>
       </div>
@@ -78,6 +82,7 @@
         <article class="tile is-child box">
           <p class="title">News Clippings</p>
             <div class="content">
+            <p><a href="https://www.forsythnews.com/sports/other-sports-news/cross-country-county-teams-take-titles-lambert-river-run/">County teams take titles at Lambert River Run</a></p>
             <p><a href="https://www.forsythnews.com/sports/other-sports-news/cross-country-south-lambert-aim-extend-countys-dominance/">South, Lambert aim to extend County's dominance</a></p>
             </div>
         </article>
@@ -90,12 +95,6 @@
       <div class="content">
         <p class="title">Upcoming Events</p>
         <div class="content events">
-        <h4>Berry College Invitational</h4>
-        <h6>Saturday, September 2nd</h6>
-        <h6><span style="color:#73000a">@ </span>Berry College</h6>
-        <h6><span style="color:#73000a">Who Races? </span>Everyone</h6>
-        <h6><a href="http://ga.milesplit.com/meets/283254/info#.WaL62Sh96Uk">Information Page</a></h6>
-        <hr>
         <h4>Afternoon Practices Begin</h4>
         <h6>Tuesday, September 5th</h6>
         <h6><span style="color:#73000a">Time: </span>3:55pm to 5:30pm</h6>
@@ -105,6 +104,7 @@
         <h6>Saturday, September 9th</h6>
         <h6><span style="color:#73000a">@ </span>Furman University</h6>
         <h6><span style="color:#73000a">Who Races? </span>Summer Running Qualifiers</h6>
+        <h6><a href="http://sc.milesplit.com/meets/265145/info#.Wa3w8siGOUk">Information Page</a></h6>
         <hr>
         <h4>Warpath Invitational</h4>
         <h6>Saturday, September 16th</h6>
