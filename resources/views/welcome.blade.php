07c9fefa143b9d35f363511ea92fe6d9fe4671b2
James <jetigue@comcast.net>
2017-09-12 12:17:59 -0400

    Updated Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,19 @@
       <div class="tile is-parent">
         <article class="tile is-child box">
           <div class="content announcements">
-            <h4>Furman Invitational</h4>
-            <p>The Furman Invitational will be this Saturday, September 9th. Summer running program athletes will be participating in the meet. <a href="https://drive.google.com/open?id=1dwQr4vpPQmENShAjkx3uD9VMXId2_ew8d_RRVDqSUIc">Summer Running Qualifiers</a></p>
-            <p>Students will be dimissed from class at 2:00 pm on Friday. The bus will depart at 2:20 pm from the stadium parking lot.</p>
-            <p><a href="http://sc.milesplit.com/meets/265145/info#.Wa3w8siGOUk">Furman Meet Information</a></p>
-            <h4>Afternoon Practices Begin!</h4>
-            <p>We will now meet in the afternoons at 3:55 pm in the stadium for all cross country practices, unless otherwise noted. Practice will last until 5:30 pm. Please arrange to be picked up from practice at that time. Be sure to stay hydrated throughout the day. Make sure you have a watch and a water bottle for practice each day.</p>
+            <h4>Warpath Invitational</h4>
+            <p>The Warpath Invitational will be this Saturday, September 16th, at Boling Park in Canton. The buses will depart from the stadium parking lot at 6:00 am. Please be there no later than 5:50 am.</p>
+            <h6>Varsity Girls (7): 8:00 am</h6>
+            <h6>Varsity Boys (7): 8:35 am</h6>
+            <h6>JV Girls (10): 9:00 am</h6>
+            <h6>JV Boys (10): 9:45 am</h6>
+            <h6>Open Girls (unlimited): 10:15 am</h6>
+            <h6>Open Boys (unlimited): 11:00 am</h6>
+            <h6>Awards: 12:30 pm</h6>
+            <p>We should return to the school around 1:45 pm</p>
+            <h4>Furman XC Classic</h4>
+            <p>Congratulations to all of the runners that competeted at the Furman XC Classic. Lambert won all four high school divisions!
+            
           </div>
         </article>
       </div>
@@ -27,16 +34,21 @@
             <article class="tile is-child box">
               <p class="title">Latest Results</p>
               <div class="content events">
+                <h4>Furman XC Classic</h4>
+                <h6><a href="http://sc.milesplit.com/meets/265145/results/537686/raw#.WbgEfsiGOUk">Varsity Boys: 1st!</a></h6>
+                <h6><a href="http://sc.milesplit.com/meets/265145/results/537684/raw#.WbgEk8iGOUk">Varsity Girls: 1st!</a></h6>
+                <h6><a href="http://sc.milesplit.com/meets/265145/results/537746/raw#.WbgEpsiGOUk">JV Boys: 1st!</a></h6>
+                <h6><a href="http://sc.milesplit.com/meets/265145/results/537716/raw#.WbgEu8iGOUk">JV Girls: 1st!</a></h6>
                 <h4>Berry Invitational</h4>
                 <h6><a href="http://ga.milesplit.com/meets/283254/results/535785/raw#.Wa31hch96Uk">Varsity Boys: 4th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/283254/results/535773/raw#.Wa31nch96Uk">Varsity Girls: 9th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/283254/results/535788/formatted#.Wa31vch96Uk">JV Boys: 5th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/283254/results/535788/formatted#.Wa31vch96Uk">JV Girls: 3rd</a></h6>
-                <h4>Lambert River Run</h4>
+<!--                 <h4>Lambert River Run</h4>
                 <h6><a href="http://ga.milesplit.com/meets/279676/results/534569/raw#.WaMAmCh96Uk">Varsity Boys: 1st!</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/279676/results/534563/raw#.WaMBfih96Uk">Varsity Girls: 4th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/279676/results/534565/raw#.WaMBnyh96Uk">JV Boys: 5th</a></h6>
-                <h6><a href="http://ga.milesplit.com/meets/279676/results/534566/raw#.WaMBxyh96Uk">JV Girls: 3rd</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/279676/results/534566/raw#.WaMBxyh96Uk">JV Girls: 3rd</a></h6> -->
               </div>
             </article>
           </div>
@@ -50,14 +62,16 @@
         <article class="tile is-child box">
           <p class="title">Girls Varsity ROW</p>
           <div class="content">
-          <p><strong>Emily Chapman</strong>, freshman, finished third for the Longhorns, running just as fast on a slower course than last week (21:15).</p>
+            <p><strong>Brianna O'Neal</strong>, junior, another top ten finish for Brianna, finishing in a time of 21:01.</p>
+<!--           <p><strong>Emily Chapman</strong>, freshman, finished third for the Longhorns, running just as fast on a slower course than last week (21:15).</p> -->
 <!--             <p><strong>Caroline Gaharan</strong>, top Longhorn finisher (6th overall) and fastest freshman in the field! (19:33.89)</p> -->
           </div>
         </article>
         <article class="tile is-child box">
           <p class="title">Girls JV ROW</p>
           <div class="content">
-            <p><strong>Kaylee Stillwell</strong>, 5th place individually with a time of 22:59 leading the Longhorns to a 3rd place team finish!</p>
+            <p><strong>Charlotte Lawson</strong>, freshman, runner-up in the girls' JV division, running a PR of 23:43!</p>
+<!--             <p><strong>Kaylee Stillwell</strong>, 5th place individually with a time of 22:59 leading the Longhorns to a 3rd place team finish!</p> -->
 <!--             <p><strong>Molly Garrett</strong>, 3rd place individually with a time of 22:47 leading the Longhorns to a 3rd place team finish!</p> -->
           </div>
         </article>
@@ -66,14 +80,16 @@
       <article class="tile is-child box">
           <p class="title">Boys Varsity ROW</p>
           <div class="content">
-           <p><strong>Adam Nicholason</strong>, finished 4th for the Longhorns and 3rd freshman in the field with a time of 17:41.</p>
+             <p><strong>Lee Keil</strong>, senior, finished 3rd overall after not being able to run for a week with a time of 17:08.</p>
+<!--            <p><strong>Adam Nicholason</strong>, finished 4th for the Longhorns and 3rd freshman in the field with a time of 17:41.</p> -->
 <!--             <p><strong>Ty Fox</strong>, individual River Run champion and school record setter in the 5k with a time of 15:35.80!</p> -->
           </div>
         </article>
         <article class="tile is-child box">
           <p class="title">Boys JV ROW</p>
           <div class="content">
-          <p><strong>Jack Frager</strong>, 18th place in the JV race with a time of 18:51.</p>
+          <p><strong>Lucas Bekele</strong>, freshman, 3rd place in the JV boys' race with a time of 18:56.</p>
+          <!-- <p><strong>Jack Frager</strong>, 18th place in the JV race with a time of 18:51.</p> -->
            <!--  <p><strong>Evan Shadoff</strong>, 15th place in the JV race with a new PR of 19:12!</p> -->
           </div>
         </article>
@@ -95,17 +111,6 @@
       <div class="content">
         <p class="title">Upcoming Events</p>
         <div class="content events">
-        <h4>Afternoon Practices Begin</h4>
-        <h6>Tuesday, September 5th</h6>
-        <h6><span style="color:#73000a">Time: </span>3:55pm to 5:30pm</h6>
-        <h6><span style="color:#73000a">Where? </span>Lambert Track</h6>
-        <hr>
-        <h4>Furman Invitational</h4>
-        <h6>Saturday, September 9th</h6>
-        <h6><span style="color:#73000a">@ </span>Furman University</h6>
-        <h6><span style="color:#73000a">Who Races? </span>Summer Running Qualifiers</h6>
-        <h6><a href="http://sc.milesplit.com/meets/265145/info#.Wa3w8siGOUk">Information Page</a></h6>
-        <hr>
         <h4>Warpath Invitational</h4>
         <h6>Saturday, September 16th</h6>
         <h6><span style="color:#73000a">@ </span>Boling Park in Canton</h6>
@@ -115,7 +120,14 @@
         <h6>Saturday, September 23rd</h6>
         <h6><span style="color:#73000a">@ </span>Sam Smith Park in Cartersville</h6>
         <h6><span style="color:#73000a">Who Races? </span>Everyone</h6>
-
+        <h4>Great American</h4>
+        <h6>Saturday, October 7th</h6>
+        <h6><span style="color:#73000a">@ </span>WakeMed Soccer Complex, Cary, NC</h6>
+        <h6><span style="color:#73000a">Who Races? </span>Top 21</h6>
+        <h4>Coach Wood Invitational</h4>
+        <h6>Saturday, October 14th</h6>
+        <h6><span style="color:#73000a">@ </span>UNG-Gainesville Campus, Gainesville, GA</h6>
+        <h6><span style="color:#73000a">Who Races? </span>Everyone</h6>
 
         </div>
       </div>
