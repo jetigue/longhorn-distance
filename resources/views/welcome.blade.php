5a7baf3a2017391148b21541541a945f21c0107a
James <jetigue@comcast.net>
2017-09-19 21:19:54 -0400

    Updated home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,18 +10,19 @@
       <div class="tile is-parent">
         <article class="tile is-child box">
           <div class="content announcements">
-            <h4>Warpath Invitational</h4>
-            <p>The Warpath Invitational will be this Saturday, September 16th, at Boling Park in Canton. The buses will depart from the stadium parking lot at 6:00 am. Please be there no later than 5:50 am.</p>
-            <h6>Varsity Girls (7): 8:00 am</h6>
-            <h6>Varsity Boys (7): 8:35 am</h6>
-            <h6>JV Girls (10): 9:00 am</h6>
-            <h6>JV Boys (10): 9:45 am</h6>
-            <h6>Open Girls (unlimited): 10:15 am</h6>
-            <h6>Open Boys (unlimited): 11:00 am</h6>
-            <h6>Awards: 12:30 pm</h6>
-            <p>We should return to the school around 1:45 pm</p>
-            <h4>Furman XC Classic</h4>
-            <p>Congratulations to all of the runners that competeted at the Furman XC Classic. Lambert won all four high school divisions!
+            <h4>Wingfoot XC Classic</h4>
+            <p>This Saturday, September 23rd, we will be competeting in the Wingfoot XC Classic. Lambert will be competing in the Championship division varsity and junior varsity races. <a href="https://www.atlantatrackclub.org/news/teams-announced-for-2017-wingfoot-xc">Wingfoot Championship Division Teams</a></p>
+            <p><strong><a href="https://www.atlantatrackclub.org/2017-wingfoot-xc-classic">
+              Atlanta Track Club Wingfoot XC Classic Meet Page</a></strong></p>
+            <p><strong>We will be departing from the stadium parking lot at 5:00 am!</strong></p>
+            <h6>JV Championsip Boys: 7:45 am</h6>
+            <h6>JV Championship Girls: 8:00 am</h6>
+            <h6>Varsity Championship Boys: 8:30 am</h6>
+            <h6>Varsity Championship Girls: 8:50 am</h6>
+            <h6>Open Boys 3k: 9:20 am</h6>
+            <h6>Open Girls 3k: 9:40 am</h6>
+            <p>We should return to the school around noon.</p>
+            
             
           </div>
         </article>
@@ -34,16 +35,24 @@
             <article class="tile is-child box">
               <p class="title">Latest Results</p>
               <div class="content events">
+                <h4>Warpath Invitational</h4>
+                <h6><a href="http://ga.milesplit.com/meets/272092/results/540279/raw#.WcG9KMh96Uk">Varsity Boys: 2nd</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/272092/results/540272/raw#.WcG9Qch96Uk">Varsity Girls: 8th</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/272092/results/540309/raw#.WcG9e8h96Uk">JV Boys: 3rd</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/272092/results/540280/raw#.WcG9lch96Uk">JV Girls: 4th</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/272092/results/540333/raw#.WcG9yMh96Uk">Open Boys: 3rd</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/272092/results/540322/raw#.WcG97sh96Uk">Open Girls: 3rd</a></h6>
+
                 <h4>Furman XC Classic</h4>
                 <h6><a href="http://sc.milesplit.com/meets/265145/results/537686/raw#.WbgEfsiGOUk">Varsity Boys: 1st!</a></h6>
                 <h6><a href="http://sc.milesplit.com/meets/265145/results/537684/raw#.WbgEk8iGOUk">Varsity Girls: 1st!</a></h6>
                 <h6><a href="http://sc.milesplit.com/meets/265145/results/537746/raw#.WbgEpsiGOUk">JV Boys: 1st!</a></h6>
                 <h6><a href="http://sc.milesplit.com/meets/265145/results/537716/raw#.WbgEu8iGOUk">JV Girls: 1st!</a></h6>
-                <h4>Berry Invitational</h4>
+<!--                 <h4>Berry Invitational</h4>
                 <h6><a href="http://ga.milesplit.com/meets/283254/results/535785/raw#.Wa31hch96Uk">Varsity Boys: 4th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/283254/results/535773/raw#.Wa31nch96Uk">Varsity Girls: 9th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/283254/results/535788/formatted#.Wa31vch96Uk">JV Boys: 5th</a></h6>
-                <h6><a href="http://ga.milesplit.com/meets/283254/results/535788/formatted#.Wa31vch96Uk">JV Girls: 3rd</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/283254/results/535788/formatted#.Wa31vch96Uk">JV Girls: 3rd</a></h6> -->
 <!--                 <h4>Lambert River Run</h4>
                 <h6><a href="http://ga.milesplit.com/meets/279676/results/534569/raw#.WaMAmCh96Uk">Varsity Boys: 1st!</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/279676/results/534563/raw#.WaMBfih96Uk">Varsity Girls: 4th</a></h6>
@@ -62,7 +71,8 @@
         <article class="tile is-child box">
           <p class="title">Girls Varsity ROW</p>
           <div class="content">
-            <p><strong>Brianna O'Neal</strong>, junior, another top ten finish for Brianna, finishing in a time of 21:01.</p>
+            <p><strong>Julia Levielle</strong>, senior, finsihed 4th for the Longhorns with a career best time of 20:42!</p>
+            <!-- <p><strong>Brianna O'Neal</strong>, junior, another top ten finish for Brianna, finishing in a time of 21:01.</p> -->
 <!--           <p><strong>Emily Chapman</strong>, freshman, finished third for the Longhorns, running just as fast on a slower course than last week (21:15).</p> -->
 <!--             <p><strong>Caroline Gaharan</strong>, top Longhorn finisher (6th overall) and fastest freshman in the field! (19:33.89)</p> -->
           </div>
@@ -70,7 +80,8 @@
         <article class="tile is-child box">
           <p class="title">Girls JV ROW</p>
           <div class="content">
-            <p><strong>Charlotte Lawson</strong>, freshman, runner-up in the girls' JV division, running a PR of 23:43!</p>
+            <p><strong>Courtney Taing</strong>, sophomore, finsihed 2nd for the Longhorns and ran a personal best of 21:16!</p>
+            <!-- <p><strong>Charlotte Lawson</strong>, freshman, runner-up in the girls' JV division, running a PR of 23:43!</p> -->
 <!--             <p><strong>Kaylee Stillwell</strong>, 5th place individually with a time of 22:59 leading the Longhorns to a 3rd place team finish!</p> -->
 <!--             <p><strong>Molly Garrett</strong>, 3rd place individually with a time of 22:47 leading the Longhorns to a 3rd place team finish!</p> -->
           </div>
@@ -80,7 +91,8 @@
       <article class="tile is-child box">
           <p class="title">Boys Varsity ROW</p>
           <div class="content">
-             <p><strong>Lee Keil</strong>, senior, finished 3rd overall after not being able to run for a week with a time of 17:08.</p>
+            <p><strong>Tanner Baker</strong>, senior, another top 20 overall finish for Tanner and a career best time of 16:32!</p>
+<!--              <p><strong>Lee Keil</strong>, senior, finished 3rd overall after not being able to run for a week with a time of 17:08.</p> -->
 <!--            <p><strong>Adam Nicholason</strong>, finished 4th for the Longhorns and 3rd freshman in the field with a time of 17:41.</p> -->
 <!--             <p><strong>Ty Fox</strong>, individual River Run champion and school record setter in the 5k with a time of 15:35.80!</p> -->
           </div>
@@ -88,7 +100,8 @@
         <article class="tile is-child box">
           <p class="title">Boys JV ROW</p>
           <div class="content">
-          <p><strong>Lucas Bekele</strong>, freshman, 3rd place in the JV boys' race with a time of 18:56.</p>
+          <p><strong>Colin Chapman</strong>, freshman, led the Longhorns to a 3rd place, placing 6th overall with a time of 16:32!</p>
+          <!-- <p><strong>Lucas Bekele</strong>, freshman, 3rd place in the JV boys' race with a time of 18:56.</p> -->
           <!-- <p><strong>Jack Frager</strong>, 18th place in the JV race with a time of 18:51.</p> -->
            <!--  <p><strong>Evan Shadoff</strong>, 15th place in the JV race with a new PR of 19:12!</p> -->
           </div>
@@ -111,22 +124,26 @@
       <div class="content">
         <p class="title">Upcoming Events</p>
         <div class="content events">
-        <h4>Warpath Invitational</h4>
-        <h6>Saturday, September 16th</h6>
-        <h6><span style="color:#73000a">@ </span>Boling Park in Canton</h6>
-        <h6><span style="color:#73000a">Who Races? </span>Everyone</h6>
         <hr>
         <h4>Wingfoot XC Classic</h4>
         <h6>Saturday, September 23rd</h6>
         <h6><span style="color:#73000a">@ </span>Sam Smith Park in Cartersville</h6>
         <h6><span style="color:#73000a">Who Races? </span>Everyone</h6>
+        <h6><strong><a href="https://www.atlantatrackclub.org/2017-wingfoot-xc-classic">Meet Website</a></strong></h6>
+        <hr>
         <h4>Great American</h4>
         <h6>Saturday, October 7th</h6>
         <h6><span style="color:#73000a">@ </span>WakeMed Soccer Complex, Cary, NC</h6>
         <h6><span style="color:#73000a">Who Races? </span>Top 21</h6>
+        <hr>
         <h4>Coach Wood Invitational</h4>
         <h6>Saturday, October 14th</h6>
         <h6><span style="color:#73000a">@ </span>UNG-Gainesville Campus, Gainesville, GA</h6>
+        <h6><span style="color:#73000a">Who Races? </span>Everyone</h6>
+        <hr>
+        <h4>Area 3 Championship</h4>
+        <h6>Saturday, October 21st</h6>
+        <h6><span style="color:#73000a">@ </span>Boling Park, Canton</h6>
         <h6><span style="color:#73000a">Who Races? </span>Everyone</h6>
 
         </div>
