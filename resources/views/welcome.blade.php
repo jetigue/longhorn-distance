19f73deae1318ff2b2feb6defa83ca16ac6927aa
James <jetigue@comcast.net>
2017-10-12 20:53:27 -0400

    update front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,15 @@
       <div class="tile is-parent">
         <article class="tile is-child box">
           <div class="content announcements">
-          <h4>Great American Cross Country Festival</h4>
-          <p>This week the team will be competing in the Great American Cross Country Festival in Cary, NC.</p>
-          <p><a href="https://www.nationalscholastic.org/gacc/info">Great American XC Festival Website</a></p>
           <h4>Coach Wood invitational</h4>
           <p>Saturday, October 14th, all runners will be participating in the Coach Wood Invitational held in Gainesville</p>
-          <p>Each runner is encouraged to bring a canned food item for the <strong>Cans for a Can</strong> food drive that day. More information about the meet will be sent out later in the week.</p>
-            
+          <p>Each runner is encouraged to bring a canned food item for the <strong>Cans for a Can</strong> food drive that day.</p>
+          <p><a href="http://cache.milesplit.com/user_files/11171/217613/coach_wood_invitational_2017_(1).pdf">Coach Wood Meet Information</a></p>
+          <h4>GaMilesplit ROW</h4>
+          <p>Congratulations to <strong>Ty Fox</strong> for being named GaMilesplit AAAAAAA Runner of the Week! Ty ran 15:47 at Great American and was the top GHSA runner that day.</p>
+          <h4>Area 3 Championship</h4>
+          <p>Saturday, October 21st, all runners will be participating in the Area 3 Championhsip at Boling Park in Canton.</p>
+          <p><a href="http://ga.milesplit.com/meets/289728/info#.WeAJl2hSyUk">Area 3 Meet Page</a></p>
           </div>
         </article>
       </div>
@@ -29,6 +31,14 @@
               <p class="title">Latest Results</p>
               <div class="content events">
                 
+                <h4>Great American XC Festival</h4>
+                <h6><a href="http://nc.milesplit.com/meets/286134/results/548976/formatted#.WeAMJmhSyUk">Championship Boys: 6th</a></h6>
+                <h6><a href="http://nc.milesplit.com/meets/286134/results/548977/formatted#.WeAMQmhSyUk">Varsity Girls: 9th</a></h6>
+                <h6><a href="http://nc.milesplit.com/meets/286134/results/548977/formatted#.WeAMQmhSyUk">Varsity Boys: 21st</a></h6>
+                <h6><a href="http://nc.milesplit.com/meets/286134/results/549019/formatted#.WeAMe2hSyUk">Varsity Girls: 26th</a></h6>
+                <h6><a href="http://nc.milesplit.com/meets/286134/results/549054/formatted#.WeAMoWhSyUk">JV Boys: 14th</a></h6>
+                <h6><a href="http://nc.milesplit.com/meets/286134/results/549070/formatted#.WeAMumhSyUk">JV Girls: NA</a></h6>
+
                 <h4>Wingfoot Classic</h4>
                 <h6><a href="http://ga.milesplit.com/meets/272824/results/543367/raw#.WdQrhGhSyUk">Varsity Boys: 4th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/272824/results/543370/raw#.WdQsG2hSyUk">Varsity Girls: 19th</a></h6>
@@ -36,13 +46,13 @@
                 <h6><a href="http://ga.milesplit.com/meets/272824/results/543364/raw#.WdQsO2hSyUk">JV Girls: 15th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/272824/results/543373/raw#.WdQsnmhSyUk">Open Boys: 8th</a></h6>
 
-                <h4>Warpath Invitational</h4>
+                <!-- <h4>Warpath Invitational</h4>
                 <h6><a href="http://ga.milesplit.com/meets/272092/results/540279/raw#.WcG9KMh96Uk">Varsity Boys: 2nd</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/272092/results/540272/raw#.WcG9Qch96Uk">Varsity Girls: 8th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/272092/results/540309/raw#.WcG9e8h96Uk">JV Boys: 3rd</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/272092/results/540280/raw#.WcG9lch96Uk">JV Girls: 4th</a></h6>
                 <h6><a href="http://ga.milesplit.com/meets/272092/results/540333/raw#.WcG9yMh96Uk">Open Boys: 3rd</a></h6>
-                <h6><a href="http://ga.milesplit.com/meets/272092/results/540322/raw#.WcG97sh96Uk">Open Girls: 3rd</a></h6>
+                <h6><a href="http://ga.milesplit.com/meets/272092/results/540322/raw#.WcG97sh96Uk">Open Girls: 3rd</a></h6> -->
 
 <!--                 <h4>Furman XC Classic</h4>
                 <h6><a href="http://sc.milesplit.com/meets/265145/results/537686/raw#.WbgEfsiGOUk">Varsity Boys: 1st!</a></h6>
@@ -72,7 +82,8 @@
         <article class="tile is-child box">
           <p class="title">Girls Varsity ROW</p>
           <div class="content">
-            <p><strong>Hannah Lee</strong>, sophomore, finished 2nd for the Longhorns with a time of 20:57!</p>
+            <p><strong>Reilly Stevens</strong>, sophomore, finished 3rd for the Longhorns with a time of 21:23!</p>
+           <!--  <p><strong>Hannah Lee</strong>, sophomore, finished 2nd for the Longhorns with a time of 20:57!</p> -->
            <!--  <p><strong>Julia Levielle</strong>, senior, finsihed 4th for the Longhorns with a career best time of 20:42!</p> -->
             <!-- <p><strong>Brianna O'Neal</strong>, junior, another top ten finish for Brianna, finishing in a time of 21:01.</p> -->
 <!--           <p><strong>Emily Chapman</strong>, freshman, finished third for the Longhorns, running just as fast on a slower course than last week (21:15).</p> -->
@@ -82,7 +93,8 @@
         <article class="tile is-child box">
           <p class="title">Girls JV ROW</p>
           <div class="content">
-             <p><strong>Caroline Harrell</strong>, junior, finished 2nd for the Longhorns with a time of 22:57!</p>
+            <p><strong>Elly Sherman</strong>, senior, finished 2nd for the Longhorns with a time of 22:23!</p>
+             <!-- <p><strong>Caroline Harrell</strong>, junior, finished 2nd for the Longhorns with a time of 22:57!</p> -->
            <!--  <p><strong>Courtney Taing</strong>, sophomore, finsihed 2nd for the Longhorns and ran a personal best of 21:16!</p> -->
             <!-- <p><strong>Charlotte Lawson</strong>, freshman, runner-up in the girls' JV division, running a PR of 23:43!</p> -->
 <!--             <p><strong>Kaylee Stillwell</strong>, 5th place individually with a time of 22:59 leading the Longhorns to a 3rd place team finish!</p> -->
@@ -94,7 +106,8 @@
       <article class="tile is-child box">
           <p class="title">Boys Varsity ROW</p>
           <div class="content">
-            <p><strong>Cooper Bocko</strong>, freshman, finished 6th for the longhorns with an impressive time of 16:59!</p>
+            <p><strong>Michael Tardella</strong>, senior, finished 4tth for the longhorns with a new PR of 16:28!</p>
+           <!--  <p><strong>Cooper Bocko</strong>, freshman, finished 6th for the longhorns with an impressive time of 16:59!</p> -->
             <!-- <p><strong>Tanner Baker</strong>, senior, another top 20 overall finish for Tanner and a career best time of 16:32!</p> -->
 <!--              <p><strong>Lee Keil</strong>, senior, finished 3rd overall after not being able to run for a week with a time of 17:08.</p> -->
 <!--            <p><strong>Adam Nicholason</strong>, finished 4th for the Longhorns and 3rd freshman in the field with a time of 17:41.</p> -->
@@ -104,7 +117,8 @@
         <article class="tile is-child box">
           <p class="title">Boys JV ROW</p>
           <div class="content">
-          <p><strong>Nick White</strong>, sophomore, finished 2nd for the Longhorns with a time of 17:41!
+          <p><strong>Alex Kleckley</strong>, sophomore, finished 1st for the Longhorns with a new PR of time of 18:40!
+          <!-- <p><strong>Nick White</strong>, sophomore, finished 2nd for the Longhorns with a time of 17:41! -->
           <!-- <p><strong>Colin Chapman</strong>, freshman, led the Longhorns to a 3rd place, placing 6th overall with a time of 16:32!</p> -->
           <!-- <p><strong>Lucas Bekele</strong>, freshman, 3rd place in the JV boys' race with a time of 18:56.</p> -->
           <!-- <p><strong>Jack Frager</strong>, 18th place in the JV race with a time of 18:51.</p> -->
@@ -116,6 +130,7 @@
         <article class="tile is-child box">
           <p class="title">News Clippings</p>
             <div class="content">
+            <p><a href="https://www.forsythnews.com/sports/other-sports-news/cross-country-south-girls-lambert-boys-put-strong-finishes-great-american-meet/">Cross country: South girls, Lambert boys put in strong finishes in Great American meet</a></p>
             <p><a href="https://www.forsythnews.com/sports/other-sports-news/cross-country-south-girls-win-wingfoot-classic-lambert-boys-finish-fourth/">Cross country: South girls win Wingfoot Classic, Lambert boys finish fourth</a></p>
             <p><a href="https://www.forsythnews.com/sports/other-sports-news/cross-country-county-teams-take-titles-lambert-river-run/">County teams take titles at Lambert River Run</a></p>
             <p><a href="https://www.forsythnews.com/sports/other-sports-news/cross-country-south-lambert-aim-extend-countys-dominance/">South, Lambert aim to extend County's dominance</a></p>
@@ -131,13 +146,8 @@
         <p class="title">Upcoming Events</p>
         <div class="content events">
         <hr>
-        <h4>Great American</h4>
-        <h6>Saturday, October 7th</h6>
-        <h6><span style="color:#73000a">@ </span>WakeMed Soccer Complex, Cary, NC</h6>
-        <h6><span style="color:#73000a">Who Races? </span>Top 21</h6>
-        <h6><a href="https://www.nationalscholastic.org/gacc/info">Website</a></h6>
 
-        <hr>
+
         <h4>Coach Wood Invitational</h4>
         <h6>Saturday, October 14th</h6>
         <h6><span style="color:#73000a">@ </span>UNG-Gainesville Campus, Gainesville, GA</h6>
@@ -148,10 +158,18 @@
         <h6><span style="color:#73000a">@ </span>Boling Park, Canton</h6>
         <h6><span style="color:#73000a">Who Races? </span>Everyone</h6>
         <hr>
+        <h4>Team Pictures</h4>
+        <h6>Team pictures will take place on Tuesday, October 24th, at 4:00 pm in the stadium.</h6>
+        <hr>
         <h4>State</h4>
         <h6>Friday, November 3rd</h6>
         <h6><span style="color:#73000a">@ </span>Carrollton Elementary, Carrollton</h6>
         <h6><span style="color:#73000a">Who Races? </span>Top 7</h6>
+        <hr>
+        <h4>Team Banquet</h4>
+        <h6>Thursday, November 16th</h6>
+        <h6><span style="color:#73000a">@ </span>Forsyth Conference Center, Lanier Tech</h6>
+        <h6><span style="color:#73000a">Time: </span>6:30 pm</h6>
 
         </div>
       </div>
