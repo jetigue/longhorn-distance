872dcbe22c466ef4f7c02e73c4f88ba547633a17
James <jetigue@comcast.net>
2018-05-16 21:38:00 -0400

    Added Camp Information

--- resources/views/main/team_camp.blade.php
+++ resources/views/main/team_camp.blade.php
@@ -1,9 +1,110 @@
 @extends('layouts.main')
 
 @section('content')
+    <h1 class="title">
+        Team Camp
+    </h1>
 
-<h1 class="title">
-  Team Camp
-</h1>
+    <div class="columns content">
+        <div class="column is-7">
+            <h4 class="has-text-weight-semibold">
+                Come help us kick off another great season by joining us for our 4th annual team camp! The goal of the
+                camp
+                is to make us a better team -- by making everyone a better distance runner through training and
+                education,
+                while at the same time bringing us closer as a group.
+            </h4>
+
+            <h4>
+                The camp will take place on the beautiful Berry College campus, which boasts miles of soft running
+                trails on one of the safest and largest college campuses in the world.
+            </h4>
+
+            <h4>
+                Like other running camps, each day will consist of some training runs, informational sessions, team
+                activities and some free time. Unlike other running camps, however, our camp will be designed with only
+                our
+                team in mind which means we can be as flexible as we want while preparing for the upcoming 2018 cross
+                country season. It promises to be a lot of fun!
+            </h4>
+        </div>
+
+        <div class="column is-5">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        2018 Team Camp
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <p><strong>Dates: </strong>Monday, July 16 - Friday, July 20</p>
+                        <p><strong>Transportation: </strong>Bus will depart from Lambert Stadium Lot at 8:00 am Monday
+                            morning and return Friday at Noon.</p>
+                        <p><strong>Cost: </strong>$325 per runner. Please make checks out to LHSXC Booster Club and turn
+                            in to Coach Tigue by Thursday, July 12.</p>
+                        <p><strong>Sign Up: </strong><a href="https://goo.gl/forms/MOMGkHAvnxn1gF3Q2">2018 Team Camp
+                                Registration</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="columns content">
+        <div class="column is-4">
+            <div class="box">
+                <img src="{{url('images/fordBuildings.jpg')}}" alt="Berry Ford Buildings">
+            </div>
+        </div>
+
+        <div class="column is-8">
+            <h4></h4>
+            <h4>The team camp will begin on the morning of Monday, July 16th and conclude on the morning of Friday, July
+                20th. This year we will be providing bus transportation to and from the camp. While on campus, the
+                runners and coaches (and any parents who wish to join us) will be staying at the Dana Residence Hall.
+                Each dorm room is equipped with two twin beds and dressers. Linens are not provided.
+            </h4>
+            <h4>The cost of the camp will be $325 for each athlete which includes 4 nights lodging, 12 meals, limited
+                accidental insurance, camp t-shirt, transportation in and around campus, and transportation to and from camp.</h4>
+        </div>
+    </div>
+
+    <div class="content columns">
+        <div class="column is-6">
+            <h4><strong>What To Bring</strong></h4>
+            <h4>Each camper should plan on bringing the following items:</h4>
+            <ul>
+                <li>Sheets for twin bed, pillow and blanket</li>
+                <li>Bath Linens</li>
+                <li>Toiletries</li>
+                <li>Training Shoes (multiple pairs if you have them)</li>
+                <li>Several sets of training clothing*</li>
+                <li>Plenty of socks</li>
+                <li>Bathing suit & towel</li>
+                <li>Preferred snacks and drinks</li>
+            </ul>
+            <p>*The dorms are equipped with laundry rooms but please bring enough running clothes to last at least a couple of days.</p>
+        </div>
+
+        <div class="column is-5 is-offset-1">
+            <h4><strong>Tentative Daily Schedule</strong></h4>
+            <p>6:30 am: Morning Run</p>
+            <p>8:00 am: Breakfast</p>
+            <p>10:30 am: Team Activities</p>
+            <p>12:00 pm: Lunch</p>
+            <p>1:00 pm: Free Time</p>
+            <p>3:00 pm: Swimming/Team Activity</p>
+            <p>5:00 pm: Easy Run</p>
+            <p>6:30 pm: Dinner</p>
+            <p>8:00 pm: Team Activities</p>
+        </div>
+    </div>
+
+    <div class="box">
+        <img src="{{url('images/volleyball.jpg')}}" alt="Berry Camp Volleyball">
+    </div>
+
+
 
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,18 @@
                 <div class="tile is-parent">
                     <article class="tile is-child box">
                         <div class="content announcements">
-                            <h4>Informational Meeting</h4>
-                            <p>Thank you fo all that attended!</p>
-                            <p>In case you didn't make it, the general information and forms can be found tot he right ->.</p>
-
                             <h4>Sign-Up for the 2018 Cross Country Season!</h4>
-                            <p>All <strong>new</strong> and <strong>returning</strong> runners will need to sign up for the 2018 season by completing the registration form below. Registering will ensure that you are added to the roster and that you are added to the email distribution list.</p>
-                            <p><a href="https://drive.google.com/open?id=1vt4fsS3plVWc6dW3XkcBxWP256HVVQc8A9PTsamQDQU">2018 Sign-Up Form</a></p>
+                            <p>All <strong>new</strong> and <strong>returning</strong> runners will need to sign up for
+                                the 2018 season by completing the registration form below. Registering will ensure that
+                                you are added to the roster and that you are added to the email distribution list.</p>
+                            <p><a href="https://drive.google.com/open?id=1vt4fsS3plVWc6dW3XkcBxWP256HVVQc8A9PTsamQDQU">2018
+                                    Team Registration Form Form</a></p>
+
+                            <h4>2018 Team Camp Information</h4>
+                            <p>Hope you can join us for this year's team camp, which will be from Monday, July 16th to
+                                Friday July 20th! This year we will be providing transportation to and from camp! More
+                                information about the camp can be found here:</p>
+                            <p><a href="{{url('https://longhorndistance.com/team_camp')}}">2018 Team Camp</a></p>
 
                         </div>
                     </article>
@@ -29,43 +34,47 @@
                             <article class="tile is-child box">
                                 <p class="title">Forms & Handouts</p>
                                 <div class="content events">
-                                    <p><a href="https://www.forsyth.k12.ga.us/page/24881">Physical Form and Information</a></p>
-                                    <p><a href="https://drive.google.com/open?id=1dQTYcAtR5ruuPmqStV3Fv4tKEwlEmmAn">Information Meeting Quick Sheet</a></p>
-                                    <p><a href="https://drive.google.com/open?id=1hJUE8Alc8j8YVE_4kycH9STHW1qRVkUc">2018 Information Packet</a></p>
-                                    <p><a href="https://drive.google.com/open?id=10_fwMsHXwNrAyDrgXlcCAdMuG6nADJ2H">2018 Meet Schedule (Draft)</a></p>
-                                    {{--<p><a href="https://drive.google.com/open?id=0B8Ca2oFHpw8HeFZMN1UwR2FEY2s">2017 Sponsor Information</a></p>--}}
-                                    {{--<p><a href="https://drive.google.com/open?id=0B8Ca2oFHpw8HYlY4bW5pSzUzSUk">2017 Spirit Wear Flyer</a></p>--}}
-                                    {{--<p><a href="https://drive.google.com/open?id=0B8Ca2oFHpw8HX0RjSmR1MENMN0k">2017 Spirit Wear Order Form</a></p>--}}
-                                    {{--<h4>Area 3 Championship</h4>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554798/raw#.Wey9GWhSzcs">Varsity--}}
-                                            {{--Boys: 3rd</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554797/raw#.Wey9KWhSzcs">Varsity--}}
-                                            {{--Girls: 4th</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554795/raw#.Wey9OWhSzcs">JV--}}
-                                            {{--Boys: 5th</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554792/raw#.Wey9UWhSzcs">JV--}}
-                                            {{--Girls: 5th</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554830/raw#.Wey9aGhSzcs">Open--}}
-                                            {{--Boys: 5th</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554814/raw#.Wey9iGhSzcs">Open--}}
-                                            {{--Girls: 4th</a></h6>--}}
+                                    <p><a href="https://www.forsyth.k12.ga.us/page/24881">Physical Form and
+                                            Information</a></p>
+                                    <p><a href="https://drive.google.com/open?id=1dQTYcAtR5ruuPmqStV3Fv4tKEwlEmmAn">Information
+                                            Meeting Quick Sheet</a></p>
+                                    <p><a href="https://drive.google.com/open?id=1hJUE8Alc8j8YVE_4kycH9STHW1qRVkUc">2018
+                                            Information Packet</a></p>
+                                    <p><a href="https://drive.google.com/open?id=10_fwMsHXwNrAyDrgXlcCAdMuG6nADJ2H">2018
+                                            Meet Schedule (Draft)</a></p>
+                                {{--<p><a href="https://drive.google.com/open?id=0B8Ca2oFHpw8HeFZMN1UwR2FEY2s">2017 Sponsor Information</a></p>--}}
+                                {{--<p><a href="https://drive.google.com/open?id=0B8Ca2oFHpw8HYlY4bW5pSzUzSUk">2017 Spirit Wear Flyer</a></p>--}}
+                                {{--<p><a href="https://drive.google.com/open?id=0B8Ca2oFHpw8HX0RjSmR1MENMN0k">2017 Spirit Wear Order Form</a></p>--}}
+                                {{--<h4>Area 3 Championship</h4>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554798/raw#.Wey9GWhSzcs">Varsity--}}
+                                {{--Boys: 3rd</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554797/raw#.Wey9KWhSzcs">Varsity--}}
+                                {{--Girls: 4th</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554795/raw#.Wey9OWhSzcs">JV--}}
+                                {{--Boys: 5th</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554792/raw#.Wey9UWhSzcs">JV--}}
+                                {{--Girls: 5th</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554830/raw#.Wey9aGhSzcs">Open--}}
+                                {{--Boys: 5th</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/289728/results/554814/raw#.Wey9iGhSzcs">Open--}}
+                                {{--Girls: 4th</a></h6>--}}
 
-                                    {{--<h4>Coach Wood Invite</h4>--}}
-                                    {{--<h6>--}}
-                                        {{--<a href="http://ga.milesplit.com/meets/278846/results/552387/formatted#.Wey8TWhSzcs">Varsity--}}
-                                            {{--Boys: 2nd</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552173/raw#.Wey8Z2hSzcs">Varsity--}}
-                                            {{--Girls: 12th</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552175/raw#.Wey8fWhSzcs">JV--}}
-                                            {{--Boys: 8th</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552179/raw#.Wey8qGhSzcs">JV--}}
-                                            {{--Girls: 6th</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552191/raw#.Wey832hSzcs">Open--}}
-                                            {{--Boys: 7th</a></h6>--}}
-                                    {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552192/raw#.Wey8wmhSzcs">Open--}}
-                                            {{--Girls: 7th</a></h6>--}}
+                                {{--<h4>Coach Wood Invite</h4>--}}
+                                {{--<h6>--}}
+                                {{--<a href="http://ga.milesplit.com/meets/278846/results/552387/formatted#.Wey8TWhSzcs">Varsity--}}
+                                {{--Boys: 2nd</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552173/raw#.Wey8Z2hSzcs">Varsity--}}
+                                {{--Girls: 12th</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552175/raw#.Wey8fWhSzcs">JV--}}
+                                {{--Boys: 8th</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552179/raw#.Wey8qGhSzcs">JV--}}
+                                {{--Girls: 6th</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552191/raw#.Wey832hSzcs">Open--}}
+                                {{--Boys: 7th</a></h6>--}}
+                                {{--<h6><a href="http://ga.milesplit.com/meets/278846/results/552192/raw#.Wey8wmhSzcs">Open--}}
+                                {{--Girls: 7th</a></h6>--}}
 
-                                    <!--          <h4>Great American XC Festival</h4>
+                                <!--          <h4>Great American XC Festival</h4>
                                                     <h6><a href="http://nc.milesplit.com/meets/286134/results/548976/formatted#.WeAMJmhSyUk">Championship Boys: 6th</a></h6>
                                                     <h6><a href="http://nc.milesplit.com/meets/286134/results/548977/formatted#.WeAMQmhSyUk">Varsity Girls: 9th</a></h6>
                                                     <h6><a href="http://nc.milesplit.com/meets/286134/results/548977/formatted#.WeAMQmhSyUk">Varsity Boys: 21st</a></h6>
@@ -110,56 +119,40 @@
 
                 </div>
             </div>
+        </div>
+            {{--<div class="tile">--}}
+                {{--<div class="tile is-parent is-vertical">--}}
+                    {{--<article class="tile is-child box">--}}
+                        {{--<p class="title">Girls Varsity ROW</p>--}}
+                        {{--<div class="content">--}}
 
-            <div class="tile">
-                <div class="tile is-parent is-vertical">
-                    <article class="tile is-child box">
-                        <p class="title">Girls Varsity ROW</p>
-                        <div class="content">
-                            <!-- <p><strong>Reilly Stevens</strong>, sophomore, finished 3rd for the Longhorns with a time of 21:23!</p> -->
-                            <!--  <p><strong>Hannah Lee</strong>, sophomore, finished 2nd for the Longhorns with a time of 20:57!</p> -->
-                            <!--  <p><strong>Julia Levielle</strong>, senior, finsihed 4th for the Longhorns with a career best time of 20:42!</p> -->
-                            <!-- <p><strong>Brianna O'Neal</strong>, junior, another top ten finish for Brianna, finishing in a time of 21:01.</p> -->
-                            <!--           <p><strong>Emily Chapman</strong>, freshman, finished third for the Longhorns, running just as fast on a slower course than last week (21:15).</p> -->
-                            <!--             <p><strong>Caroline Gaharan</strong>, top Longhorn finisher (6th overall) and fastest freshman in the field! (19:33.89)</p> -->
-                        </div>
-                    </article>
-                    <article class="tile is-child box">
-                        <p class="title">Girls JV ROW</p>
-                        <div class="content">
-                            <!-- <p><strong>Elly Sherman</strong>, senior, finished 2nd for the Longhorns with a time of 22:23!</p> -->
-                            <!-- <p><strong>Caroline Harrell</strong>, junior, finished 2nd for the Longhorns with a time of 22:57!</p> -->
-                            <!--  <p><strong>Courtney Taing</strong>, sophomore, finsihed 2nd for the Longhorns and ran a personal best of 21:16!</p> -->
-                            <!-- <p><strong>Charlotte Lawson</strong>, freshman, runner-up in the girls' JV division, running a PR of 23:43!</p> -->
-                            <!--             <p><strong>Kaylee Stillwell</strong>, 5th place individually with a time of 22:59 leading the Longhorns to a 3rd place team finish!</p> -->
-                            <!--             <p><strong>Molly Garrett</strong>, 3rd place individually with a time of 22:47 leading the Longhorns to a 3rd place team finish!</p> -->
-                        </div>
-                    </article>
-                </div>
-                <div class="tile is-parent is-vertical">
-                    <article class="tile is-child box">
-                        <p class="title">Boys Varsity ROW</p>
-                        <div class="content">
-                            <!-- <p><strong>Michael Tardella</strong>, senior, finished 4tth for the longhorns with a new PR of 16:28!</p> -->
-                            <!--  <p><strong>Cooper Bocko</strong>, freshman, finished 6th for the longhorns with an impressive time of 16:59!</p> -->
-                            <!-- <p><strong>Tanner Baker</strong>, senior, another top 20 overall finish for Tanner and a career best time of 16:32!</p> -->
-                            <!--              <p><strong>Lee Keil</strong>, senior, finished 3rd overall after not being able to run for a week with a time of 17:08.</p> -->
-                            <!--            <p><strong>Adam Nicholason</strong>, finished 4th for the Longhorns and 3rd freshman in the field with a time of 17:41.</p> -->
-                            <!--             <p><strong>Ty Fox</strong>, individual River Run champion and school record setter in the 5k with a time of 15:35.80!</p> -->
-                        </div>
-                    </article>
-                    <article class="tile is-child box">
-                        <p class="title">Boys JV ROW</p>
-                        <div class="content">
-                            <!-- <p><strong>Alex Kleckley</strong>, sophomore, finished 1st for the Longhorns with a new PR of time of 18:40!</p> -->
-                            <!-- <p><strong>Nick White</strong>, sophomore, finished 2nd for the Longhorns with a time of 17:41! -->
-                            <!-- <p><strong>Colin Chapman</strong>, freshman, led the Longhorns to a 3rd place, placing 6th overall with a time of 16:32!</p> -->
-                            <!-- <p><strong>Lucas Bekele</strong>, freshman, 3rd place in the JV boys' race with a time of 18:56.</p> -->
-                            <!-- <p><strong>Jack Frager</strong>, 18th place in the JV race with a time of 18:51.</p> -->
-                            <!--  <p><strong>Evan Shadoff</strong>, 15th place in the JV race with a new PR of 19:12!</p> -->
-                        </div>
-                    </article>
-                </div>
+
+                        {{--</div>--}}
+                    {{--</article>--}}
+                    {{--<article class="tile is-child box">--}}
+                        {{--<p class="title">Girls JV ROW</p>--}}
+                        {{--<div class="content">--}}
+
+
+                        {{--</div>--}}
+                    {{--</article>--}}
+                {{--</div>--}}
+                {{--<div class="tile is-parent is-vertical">--}}
+                    {{--<article class="tile is-child box">--}}
+                        {{--<p class="title">Boys Varsity ROW</p>--}}
+                        {{--<div class="content">--}}
+
+
+                        {{--</div>--}}
+                    {{--</article>--}}
+                    {{--<article class="tile is-child box">--}}
+                        {{--<p class="title">Boys JV ROW</p>--}}
+                        {{--<div class="content">--}}
+
+
+                        {{--</div>--}}
+                    {{--</article>--}}
+                {{--</div>--}}
                 <div class="tile is-parent">
                     <article class="tile is-child box">
                         <p class="title">News Clippings</p>
@@ -197,6 +190,11 @@
                         <h6>Tuesday, June 5th</h6>
                         <h6><span style="color:#73000a">@ </span>Lambert Track</h6>
                         <h6><span style="color:#73000a">Time: </span>6:30 am</h6>
+                        <hr>
+                        <h4>Team Camp</h4>
+                        <h6>July 16 - July 20</h6>
+                        <h6><span style="color:#73000a">@ </span>Berry College</h6>
+                        <h6><a href="{{url('https://longhorndistance.com/team_camp')}}">Camp Information</a></h6>
 
                     </div>
                 </div>
